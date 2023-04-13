a08668e8ded9009252db6780b67e9c34bd16626d
mohmedahmed11 <albrer112@gmail.com>
2023-04-13 23:59:54 +0200

    setup Order pricing page with required options and links

--- Shahn/Scene/Orders/OrderPricingViewController.swift
+++ Shahn/Scene/Orders/OrderPricingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  OrdersViewController.swift
+//  OrderPricingViewController.swift
 //  Shahn
 //
 //  Created by Mohamed Ahmed on 4/13/23.
@@ -7,16 +7,24 @@
 
 import UIKit
 
-class OrdersViewController: UIViewController {
+class OrderPricingViewController: UIViewController {
     
+    @IBOutlet weak var optionsSegment: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.customNavJannaFont()
+        setupSegments()
         // Do any additional setup after loading the view.
     }
     
+    func setupSegments() {
+        let seperatorImage = UIImage(named: "line")
+        optionsSegment.setDividerImage(seperatorImage, forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+        
+        let fontAttributes = [NSAttributedString.Key.font: UIFont(name: "BahijJanna", size: 16) ?? .systemFont(ofSize: 16)]
+        optionsSegment.setTitleTextAttributes(fontAttributes, for: .normal)
+    }
 
     /*
     // MARK: - Navigation
@@ -30,7 +38,7 @@ class OrdersViewController: UIViewController {
 
 }
 
-extension OrdersViewController: UITableViewDelegate, UITableViewDataSource {
+extension OrderPricingViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
@@ -41,6 +49,6 @@ extension OrdersViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.performSegue(withIdentifier: "orderPrices", sender: nil)
+        self.performSegue(withIdentifier: "providerDetails", sender: nil)
     }
 }
