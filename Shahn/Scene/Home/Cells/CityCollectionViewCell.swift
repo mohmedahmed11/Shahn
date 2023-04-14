9928a5e4f377096b09c667e9fac7fe53bf688b65
mohmedahmed11 <albrer112@gmail.com>
2023-04-14 12:14:36 +0200

    setting cities fetch in providers page

---
+++ Shahn/Scene/Home/Cells/CityCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CityCollectionViewCell.swift
+//  Shahn
+//
+//  Created by Mohamed Ahmed on 4/14/23.
+//
+
+import UIKit
+
+class CityCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var name: UILabel!
+}
--- Shahn/Scene/Home/PrividersViewController.swift
+++ Shahn/Scene/Home/PrividersViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ProvidersViewController: UIViewController {
     
@@ -15,13 +16,31 @@ class ProvidersViewController: UIViewController {
     @IBOutlet weak var citiesCollectionView: UICollectionView!
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var presenter: HomePesenter?
+    
+    var cities = [JSON]()
+    var providers = [JSON]()
+    var selectedCategory: JSON!
+    
+    var selectedCityIndex = 0
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        presenter = HomePesenter(self)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        citiesCollectionView.transform = CGAffineTransform(scaleX: -1, y: 1)
         self.setupSearchView()
         self.setupSegments()
-        
+        self.loadAll()
         // Do any additional setup after loading the view.
+    }
+    
+    func loadAll() {
+        self.presenter?.loadCities()
     }
     
     func setupSearchView() {
@@ -57,6 +76,44 @@ class ProvidersViewController: UIViewController {
 
 }
 
+extension ProvidersViewController: ProvidersViewDelegate {
+    func didReciveCities(with result: Result<JSON, Error>) {
+        switch result {
+        case .success(let data):
+            if data["operation"].boolValue == true {
+                self.cities = data["data"].arrayValue
+                if !self.cities.isEmpty {
+                    self.citiesCollectionView.reloadData()
+                }else {
+                    self.citiesCollectionView.isHidden = true
+                }
+            }else {
+                self.citiesCollectionView.isHidden = true
+            }
+        case .failure(let error):
+            self.faildLoading(icon: UIImage(named: "reload"), content: "حدث خطأ اعد المحاولة")
+            print(error)
+        }
+    }
+    
+    func addNoData() {
+        let error = noDataFoundNip()
+        error.frame.size.height = 200
+        error.content.text = "لا يوجد مقدمي خدمات متاحين"
+        self.tableView.backgroundView = error
+    }
+    
+    func faildLoading(icon: UIImage?, content: String) {
+        let error = noUserDataNotLoadedNip(icon, content)
+        error.frame = self.tabBarController!.tabBar.frame
+        error.reloadData = {
+            self.loadAll()
+            error.removeFromSuperview()
+        }
+        self.tabBarController?.view.addSubview(error)
+    }
+}
+
 extension ProvidersViewController: UISearchBarDelegate, UISearchTextFieldDelegate{
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         showSearchbarCancelBtn(status: false)
@@ -83,14 +140,45 @@ extension ProvidersViewController: UISearchBarDelegate, UISearchTextFieldDelegat
     }
 }
 
-extension ProvidersViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension ProvidersViewController: UICollectionViewDelegate, UICollectionViewDataSource , UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return cities.count + 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CityCollectionViewCell
+        cell.transform = CGAffineTransform(scaleX: -1, y: 1)
+        if indexPath.item != 0 {
+            cell.name.text = cities[indexPath.item - 1]["name"].string
+        }else {
+            cell.name.text = "الكل"
+        }
+        
+        if selectedCityIndex == indexPath.item {
+            cell.name.backgroundColor = .systemBackground
+        }else {
+            cell.name.backgroundColor = .clear
+        }
+        
+        cell.name.layer.cornerRadius = 4
+        cell.name.clipsToBounds = true
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var width: CGFloat = 80
+        
+        if indexPath.item != 0 {
+            let str = self.cities[indexPath.item - 1]["name"].stringValue
+            width = str.size(height: 34, font: .systemFont(ofSize: 18)).width + 16
+        }
+        
+        return CGSize(width: width, height: 34)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.selectedCityIndex = indexPath.item
+        self.citiesCollectionView.reloadData()
     }
 }
 
